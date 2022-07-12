1c6bc26f88beda97baf6a504ac9cfab93628fad3
imrandev20 <imrandev20@gmail.com>
2022-07-12 15:05:32 +0600

    added background image

--- src/components/AppointmentModal.js
+++ src/components/AppointmentModal.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { FaTimes } from "react-icons/fa";
-import Form from "./form";
+import Form from "./Form";
 
 const AppointmentModal = ({ showModal, setShowModal }) => {
   console.log(showModal);
